62a230b35fdf6024f9c3ef5f4374c1a9a4599b9a
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-09-04 15:43:15 -0300

    feat: notes tests

--- test/notes.test.e2e-spec.ts
+++ test/notes.test.e2e-spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { E2EUtils } from './utils/e2e.utils';
-import { faker } from '@faker-js/faker';
+import { fa, faker } from '@faker-js/faker';
 import { UserFactory } from './factories/user.factory';
 import * as request from 'supertest';
 import { JwtService } from '@nestjs/jwt';
@@ -39,6 +39,58 @@ describe('Auth E2E Tests (e2e)', () => {
     await app.init();
 
     await E2EUtils.cleanDb(prisma);
+  });
+
+  it('POST /notes => should return Bad Request if body is invalid', async () => {
+    const note: CreateNoteDto = new CreateNoteDto({
+      title: faker.lorem.word(),
+    });
+
+    const user = await new UserFactory(prisma).persist();
+    const token = await E2EUtils.authenticate(jwt, user);
+
+    const response = await request(app.getHttpServer())
+      .post('/notes')
+      .set('Authorization', `Bearer ${token.token}`)
+      .send({
+        ...note,
+      });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('POST /notes => should return Conflict if user already has a note with the same title', async () => {
+    const user = await new UserFactory(prisma).persist();
+    const token = await E2EUtils.authenticate(jwt, user);
+    const note = await new NotesFactory(prisma).persist(user);
+
+    const response = await request(app.getHttpServer())
+      .post('/notes')
+      .set('Authorization', `Bearer ${token.token}`)
+      .send({
+        title: note.title,
+        content: faker.lorem.sentence(),
+      });
+
+    expect(response.status).toBe(409);
+  });
+
+  it('POST /notes => should return 201 even if another user already has a note with the same title', async () => {
+    const user = await new UserFactory(prisma).persist();
+    const note = await new NotesFactory(prisma).persist(user);
+
+    const anotherUser = await new UserFactory(prisma).persist();
+    const anotherToken = await E2EUtils.authenticate(jwt, anotherUser);
+
+    const response = await request(app.getHttpServer())
+      .post('/notes')
+      .set('Authorization', `Bearer ${anotherToken.token}`)
+      .send({
+        title: note.title,
+        content: faker.lorem.sentence(),
+      });
+
+    expect(response.status).toBe(201);
   });
 
   it('POST /notes => should return 201', async () => {
@@ -82,6 +134,36 @@ describe('Auth E2E Tests (e2e)', () => {
     ]);
   });
 
+  it('GET /notes/:id => should return Not Found if credential does not exist', async () => {
+    const user = await new UserFactory(prisma).persist();
+    const token = await E2EUtils.authenticate(jwt, user);
+    const note = await new NotesFactory(prisma).persist(user);
+    await prisma.notes.delete({
+      where: {
+        id: note.id,
+      },
+    });
+
+    const response = await request(app.getHttpServer())
+      .get(`/notes/${note.id}`)
+      .set('Authorization', `Bearer ${token.token}`);
+
+    expect(response.status).toBe(404);
+  });
+
+  it('GET /notes/:id => should return Forbidden if credential belongs to another user', async () => {
+    const user = await new UserFactory(prisma).persist();
+    const note = await new NotesFactory(prisma).persist(user);
+    const anotherUser = await new UserFactory(prisma).persist();
+    const token = await E2EUtils.authenticate(jwt, anotherUser);
+
+    const response = await request(app.getHttpServer())
+      .get(`/notes/${note.id}`)
+      .set('Authorization', `Bearer ${token.token}`);
+
+    expect(response.status).toBe(403);
+  });
+
   it('GET /notes/:id => should return 200 and credential', async () => {
     const user = await new UserFactory(prisma).persist();
     const token = await E2EUtils.authenticate(jwt, user);
@@ -101,7 +183,37 @@ describe('Auth E2E Tests (e2e)', () => {
     });
   });
 
-  it('DELETE /credentials/:id => should delete a credential', async () => {
+  it('DELETE /notes/:id => should return Not Found if credential does not exist', async () => {
+    const user = await new UserFactory(prisma).persist();
+    const token = await E2EUtils.authenticate(jwt, user);
+    const note = await new NotesFactory(prisma).persist(user);
+    await prisma.notes.delete({
+      where: {
+        id: note.id,
+      },
+    });
+
+    const response = await request(app.getHttpServer())
+      .delete(`/notes/${note.id}`)
+      .set('Authorization', `Bearer ${token.token}`);
+
+    expect(response.status).toBe(404);
+  });
+
+  it('DELETE /notes/:id => should return Forbidden if credential belongs to another user', async () => {
+    const user = await new UserFactory(prisma).persist();
+    const note = await new NotesFactory(prisma).persist(user);
+    const anotherUser = await new UserFactory(prisma).persist();
+    const token = await E2EUtils.authenticate(jwt, anotherUser);
+
+    const response = await request(app.getHttpServer())
+      .delete(`/notes/${note.id}`)
+      .set('Authorization', `Bearer ${token.token}`);
+
+    expect(response.status).toBe(403);
+  });
+
+  it('DELETE /notes/:id => should delete a credential', async () => {
     const user = await new UserFactory(prisma).persist();
     const token = await E2EUtils.authenticate(jwt, user);
     const note = await new NotesFactory(prisma).persist(user);
@@ -112,11 +224,11 @@ describe('Auth E2E Tests (e2e)', () => {
 
     expect(response.status).toBe(200);
 
-    const deletedCredential = await prisma.notes.findUnique({
+    const deletedNote = await prisma.notes.findUnique({
       where: {
         id: note.id,
       },
     });
-    expect(deletedCredential).toBeNull();
+    expect(deletedNote).toBeNull();
   });
 });
